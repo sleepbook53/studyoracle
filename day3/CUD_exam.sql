b098b52eccdc91cf17514927b50842bc469885a7
sleepbook53 <sleepbook53@gmail.com>
2022-01-13 15:03:25 +0900

    day3 join

---
+++ day3/CUD_exam.sql
@@ -0,0 +1,85 @@
+--데이터 입력 INSERT
+INSERT INTO BONUS
+        ( ename
+        , job
+        , sal
+        , comm)
+  VALUES
+       ( 'JACK'
+       , 'SALEMAN'
+       , 4000
+       , NULL);
+  COMMIT; -- 완전저장
+  ROLLBACK; -- 취소 / SLEECT는 COMMIT 안함(INSERT DELATE UPDATE는 해야함)
+  
+  
+-- TEST 테이블 입력 쿼리
+INSERT INTO test
+        (idx, title, descs)
+VALUES
+        (1, NULL, '내용증명');
+        --- idx와 title은 not NULL
+
+INSERT INTO test
+        (idx, title, descs)
+VALUES
+        (1, '내용증명', NULL); --- 이 상황에서는 한번에 데이터 하나만/ idx에 중복도 안됨
+        INSERT INTO test
+        (idx, title, descs)
+VALUES
+        (2, '내용증명2', NULL);
+INSERT INTO test
+        (idx, title, descs, regatetime)        
+VALUES
+        (3, '내용증명3','내용내용내용', SYSDATE);
+        
+        
+INSERT INTO test
+        (idx, title, descs, regatetime)        
+VALUES
+        (4, '내용증명4','내용내용내용내용', TO_DATE('2021-12-31', 'yyyy-mm-dd'));
+        
+-- 시퀀스(오라클에만 있는 것)
+---- 뭔가 만들겠다: 크레이트 명령어 CREATE
+SELECT SEQ_TEST. CURRVAL FROM dual; -- 현재 값보기
+SELECT SEQ_TEST. NEXTVAL FROM dual; -- 다음 값보기
+
+INSERT INTO test
+        (idx, title, descs, regatetime)        
+VALUES
+        (SEQ_TEST. NEXTVAL, '내용증명6','내용내용내용6', SYSDATE); -- 숫자 자동증가
+--- 시퀀스는 테이블과 독립적이라 복수로 사용 가능
+INSERT INTO test
+        (idx, title, descs, regatetime)        
+VALUES
+        (SEQ_TEST. NEXTVAL, '내용증명6','내용내용내용6', SYSDATE); -- 숫자 자동증가
+        
+        
+-- UPDATE **PK값은 어지간하면 수정하지 않음
+UPDATE test
+  SET title = '내용증명요?'
+    , descs = '내용증명됩니다?'
+  WHERE idx = 6 ;
+
+--DELETE
+DELETE FROM test
+  WHERE idx = 1;
+
+
+--서브쿼리 => 쿼리 안의 쿼리
+SELECT ROWNUM
+      , su.ename
+      , su.job
+      , su.sal
+      , su.comm
+  FROM 
+(SELECT ename
+      , job
+      , sal
+      , comm
+  FROM emp
+  ORDER BY sal DESC
+) su
+WHERE ROWNUM <= 1;
+
+SELECT ROWNUM, idx, title FROM test;
--- day3/join_exam.sql
+++ day3/join_exam.sql
@@ -1,4 +1,5 @@
 --JOIN
+--INNER JOIN
 SELECT e. empno
      , e. ename
      , e. job
@@ -7,16 +8,92 @@ SELECT e. empno
      , d. dname
   FROM emp e
   INNER JOIN dept d
-    ON e. deptno = d. deptno;-- INNER JOIN의 조건을 만들어줌
+    ON e. deptno = d. deptno;-- ON, INNER JOIN의 조건을 만들어줌
   
   -- selection
   SELECT e. empno
      , e. ename
      , e. job
      , TO_CHAR(e.hiredate, 'yyyy-mm-dd') hiredate
-     , e.deptno
-     , d.dname
+     , e. deptno
+     , d. dname
   FROM emp e
   INNER JOIN dept d
     ON e. deptno = d. deptno
   WHERE e. JOB = 'SALESMAN';
+  
+  
+  --PLSQL INNER JOIN 방식
+SELECT e. empno
+     , e. ename
+     , e. job
+     , TO_CHAR(e.hiredate, 'yyyy-mm-dd') hiredate
+     , e. deptno
+     , d. dname
+  FROM emp e, dept d 
+  WHERE  e. deptno = d. deptno -- tip. WHERE 1 = 1 로 해도 문제 없음
+     AND e. JOB = 'SALESMAN';
+  --- 개발자들이 inner join 귀찮아서 where씀.(join과 on은 함께)
+  --- PLSQL과 MLSQL에서 이런 방식 함께 사용함
+ 
+ -- LEFE OUTER JOIN
+ SELECT e. empno
+     , e. ename
+     , e. job
+     , TO_CHAR(e.hiredate, 'yyyy-mm-dd') hiredate
+     , e. deptno
+     , d. dname
+  FROM emp e
+  LEFT OUTER JOIN dept d
+    ON e. deptno = d. deptno;
+-- RIGHT OUTER JOIN
+SELECT e. empno
+     , e. ename
+     , e. job
+     , TO_CHAR(e.hiredate, 'yyyy-mm-dd') hiredate
+     , e. deptno
+     , d. dname
+  FROM emp e
+  RIGHT OUTER JOIN dept d
+    ON e. deptno = d. deptno;
+  --- LEFT와 RIGHT의 기준점에 따라 값이 달라질 수 있음.
+  --- 기준점보다 데이터가 많으면 기준점 만큼만 도출
+  --- 기준점보다 데이터가 적으면 NULL값으로 도출
+  
+  -- 오라클만의 OUTTER JOIN 문법
+  SELECT e. empno
+     , e. ename
+     , e. job
+     , TO_CHAR(e.hiredate, 'yyyy-mm-dd') hiredate
+     , e. deptno
+     , d. dname
+  FROM emp e, dept d
+  WHERE e. deptno = d. deptno(+); --PLSQL LEFT OUTER JOIN
+  ---WHERE e. deptno(+) = d. deptno); --PLSQL RIGHT OUTER JOIN
+  
+  --3개 테이블 INNER JOIN
+   SELECT e. empno
+     , e. ename
+     , e. job
+     , TO_CHAR(e.hiredate, 'yyyy-mm-dd') hiredate
+     , e. deptno
+     , d. dname
+     , b. comm
+  FROM emp e, dept d, bonus b
+  WHERE e. deptno = d. deptno
+  AND e.ename = b. ename;
+  
+  ----3개 테이블 OUTER JOIN
+   SELECT e. empno
+     , e. ename
+     , e. job
+     , TO_CHAR(e.hiredate, 'yyyy-mm-dd') hiredate
+     , e. deptno
+     , d. dname
+     , b. comm
+  FROM emp e, dept d, bonus b
+  WHERE e. deptno(+) = d. deptno
+  AND e. ename = b. ename(+);
+  
+  
+  
